328e5b7713a23ba6f2f9273ee3f5e71d37f76b3b
Kurpenov Kuat <kurpenov2003@yandex.ru>
2022-09-13 23:53:24 +0600

    Added move mech to algorithm

--- 0088_merge_sorted_array.py
+++ 0088_merge_sorted_array.py
@@ -1,9 +1,14 @@
+#! /usr/bin/python3.10
+
 from typing import List
 
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         insert_index = 0
-        for i in range(m):
+        for i in range(n + m):
             if nums2[insert_index] < nums1[i]:
-                nums2[]
+                for j in range(n + m - 1, i + 1, -1):
+                    nums1[j] = nums1[j - 1]
+                nums1[i] = nums2[insert_index]
+                insert_index += 1
