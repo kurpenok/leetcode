91d4093b4bc3f3d2d21975380b21a6b8364992ff
Kurpenov Kuat <kurpenov2003@yandex.ru>
2022-09-30 20:59:51 +0600

    Added merge sorted array task solution

--- 0088_merge_sorted_array.py
+++ 0088_merge_sorted_array.py
@@ -5,10 +5,16 @@ from typing import List
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        insert_index = 0
-        for i in range(n + m):
-            if nums2[insert_index] < nums1[i]:
-                for j in range(n + m - 1, i + 1, -1):
-                    nums1[j] = nums1[j - 1]
-                nums1[i] = nums2[insert_index]
-                insert_index += 1
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                k -= 1
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                k -= 1
+                j -= 1
