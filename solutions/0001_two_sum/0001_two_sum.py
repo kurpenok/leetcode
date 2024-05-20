482b99147b413ac7c43825e2468b8347843b8a7c
Kurpenov Kuat <kurpenov2003@yandex.ru>
2024-05-20 23:20:08 +0600

    Updated code style in two sum solution

--- solutions/0001_two_sum/0001_two_sum.py
+++ solutions/0001_two_sum/0001_two_sum.py
@@ -1,10 +1,14 @@
+from typing import List
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         h = {}
         for i, n in enumerate(nums):
-            h[n] = i;
-        
+            h[n] = i
+
         for i, n in enumerate(nums):
             if target - n in h and i != h[target - n]:
                 return [i, h[target - n]]
 
+        return []
