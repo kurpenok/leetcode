4bbb72fd47d94759f18c218407e323f6bd4908f7
Kuat Kurpenov <kurpenov2003@yandex.ru>
2024-09-03 18:48:55 +0600

    Added solution of 766 problem

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -97,3 +97,4 @@ pub mod s0744_find_smallest_letter_greater_than_target;
 pub mod s0747_largest_number_at_least_twice_of_others;
 pub mod s0748_shortest_completing_word;
 pub mod s0762_prime_number_of_set_bits_in_binary_representation;
+pub mod s0766_toeplitz_matrix;
