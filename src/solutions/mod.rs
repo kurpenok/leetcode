38750a5e180cd13aaeb6063b7b8c9d90f37d5af8
Kuat Kurpenov <kurpenov2003@yandex.ru>
2024-12-13 13:04:04 +0600

    Added solution of 1287 problem

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -169,4 +169,5 @@ pub mod s1103_distribute_candies_to_people;
 pub mod s1108_defanging_an_ip_address;
 pub mod s1122_relative_sort_array;
 pub mod s1160_find_words_that_can_be_formed_by_characters;
+pub mod s1287_element_appearing_more_than_25_in_sorted_array;
 pub mod s1619_mean_of_array_after_removing_some_elements;
