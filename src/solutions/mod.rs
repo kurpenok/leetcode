ad6b61ae7b63d8b0a76b48a69f5d8b8b881d2088
Kuat Kurpenov <kurpenov2003@yandex.ru>
2025-01-30 13:50:06 +0600

    Added solution of 24 problem

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -17,6 +17,7 @@ pub mod s0017_letter_combinations_of_a_phone_number;
 pub mod s0018_4sum;
 pub mod s0019_remove_nth_node_from_end_of_list;
 pub mod s0022_generate_parentheses;
+pub mod s0024_swap_nodes_in_pairs;
 pub mod s0231_power_of_two;
 pub mod s0232_implement_queue_using_stacks;
 pub mod s0234_palindrome_linked_list;
