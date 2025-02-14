278983ec150e70a79909de676ec98739a4e231cc
Kuat Kurpenov <kurpenov2003@yandex.ru>
2025-02-14 08:05:04 +0600

    Added solution of problem 53

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -32,6 +32,7 @@ pub mod s0046_permutations;
 pub mod s0047_permutations_ii;
 pub mod s0048_rotate_image;
 pub mod s0049_group_anagrams;
+pub mod s0053_maximum_subarray;
 pub mod s0231_power_of_two;
 pub mod s0232_implement_queue_using_stacks;
 pub mod s0234_palindrome_linked_list;
