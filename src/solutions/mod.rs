cc7f0cd3c32db4922df54ae8601864e836c8f71b
Kuat Kurpenov <kurpenov2003@yandex.ru>
2025-04-06 21:32:18 +0600

    Added solution of problem 1480

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -300,6 +300,7 @@ pub mod s1460_make_two_arrays_equal_by_reversing_subarrays;
 pub mod s1464_maximum_product_of_two_elements_in_an_array;
 pub mod s1470_shuffle_the_array;
 pub mod s1475_final_prices_with_a_special_discount_in_a_shop;
+pub mod s1480_running_sum_of_1d_array;
 pub mod s1507_reformat_date;
 pub mod s1550_three_consecutive_odds;
 pub mod s1619_mean_of_array_after_removing_some_elements;
